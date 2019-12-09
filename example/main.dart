5751463355eb1013a2ba1ea04a45cecccf22b315
Luc van der Zandt <luc@Lucs-MacBook-Pro.local>
2019-12-09 12:36:26 +0100

    Added application exit in example

--- example/main.dart
+++ example/main.dart
@@ -4,7 +4,8 @@ import 'package:messagebird/balance.dart';
 
 void main() {
   final BalanceService balanceService = ApiBalanceService('YOUR_KEY_HERE');
-  balanceService
-      .read()
-      .then((balance) => stdout.write('My balance is ${balance.amount}'));
+  balanceService.read().then((balance) {
+    stdout.write('My balance is ${balance.amount}');
+    exit(0);
+  });
 }
